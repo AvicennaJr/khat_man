030b9766351e03a604190e98b1cf905581047041
AvicennaJr <87450618+AvicennaJr@users.noreply.github.com>
2021-07-15 01:10:07 +0300

    Add files via upload

---
+++ src/buttons/touchbuttons.gd
@@ -0,0 +1,19 @@
+extends Node2D
+#
+#
+#NO NEED FOR THIS SCRIPT
+
+#func _on_left_pressed():
+#	Input.is_action_pressed("ui_left")
+#
+#
+#func _on_right_pressed():
+#	Input.is_action_pressed("ui_right")
+#
+#
+#func _on_jump_pressed():
+#	Input.is_action_just_pressed("ui_select")
+#
+#
+#func _on_jump_released():
+# Input.is_action_just_released("ui_select")
